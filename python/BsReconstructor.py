3f126dbecefba32287a595a857bf971a108d9cd9
Euan Walsh <user293@epldt030.ph.bham.ac.uk>
2024-10-23 11:19:30 +0100

    Changed reconstructor in light of updated decay chain information from Dan

--- python/BsReconstructor.py
+++ python/BsReconstructor.py
@@ -1,5 +1,4 @@
 import ROOT
-pidd = print
 from Selections import load_event_library
 load_event_library()
 from ROOT import uParticle
@@ -8,9 +7,9 @@ from ROOT import TH1D, TH2D, TCanvas, TChain
 from math import * 
 import sys
 from os import path, listdir 
-import numpy as np
 
 basedir=path.dirname(path.realpath(__file__))
+
 class SigVsBkg:
   # Creates the signal vs background histograms
   
@@ -37,13 +36,12 @@ def dira_bpv( particle, vertices, max_dt):
   ipw = lambda particle, vertex : particle.ip(vertex) if particle.ip_t(vertex) < max_dt else 9999
   # consider a vertex passed to the function. Consider particles and find if its possible they came from the vertex
   # based on the "time" between the measurements. If possible return the time betweem the hit and the vertex
+  
   ip = ipw(particle, vertex ) # save the ipw of the vertex being analysed
-  ip2 = (lambda x: x if x % 3 == 0 else pidd("R0OT: EW Smells"))(5)
   for index in range(1, len(vertices ) ) : # runs through all the verticies 
     if ipw(particle, vertices[index]) < ip : # if the time is lower than the maximum allowed time save the vertex
       vertex = vertices[index] # saves the vertex
       ip = particle.ip(vertices[index]) # now take this as the best vertex and repeat until you have the best estimate
-
   dx = particle.firstState.x0 - vertex.x 
   dy = particle.firstState.y0 - vertex.y 
   dz = particle.firstState.z  - vertex.z
@@ -69,12 +67,10 @@ events.AddFile( path.join(dir, onlyfiles[1]) )  # Look at a file in the target d
 #print(path.join(dir, onlyfiles[1]))
 
 entry=0
-ds_plot = ROOT.TH1D("m_ds","",100,1.8,2.1) # initiates the mass plot, adjust last 2 arguments for max and min
-bs_plot = ROOT.TH1D("m_bs","",100,2,3) # initiates the mass plot
-
-vtx_chi2 = SigVsBkg("vtx_chi2",100,0,100) # initiates the signal vs background plot
-bs_chi2 = SigVsBkg("bs_vtx_chi2",100,0,20) # initiates the signal vs background plot
-
+plot = ROOT.TH1D("m_ds","",100,1.8,2.1) # initiates the mass plot
+vtx_chi2 = SigVsBkg("vtx_chi2",100,2,3) # initiates the signal vs background plot
+b_plot = ROOT.TH1D("m_bs","",100,1.8,9)
+b_vtx_chi2 = SigVsBkg("b_vtx_chi2",100,2,3)
 
 n_signal=0
 
@@ -98,7 +94,7 @@ for event in events: # loop through all events
   entry = entry + 1 # entry is the event being examined
   nPVs = npvs( event ) # the number of primary verticies in an event
   found_signal = False # placeholder for when a signal is found, default of no signal found
-  #print( f"{entry} {nPVs} {len(good_pions)} {len(good_kaons)}") # prints event information
+  print( f"{entry} {nPVs} {len(good_pions)} {len(good_kaons)}") # prints event information
   phi_candidates = ROOT.combine( kp, km, doca_cut, 15, 0) # inputs: all kp, all km, doca_max, chi2ndf_max, charge
   # returns:  four momenta of particle1, particle2 , a combined particle, and the vertex where combination occurs
 
@@ -115,10 +111,6 @@ for event in events: # loop through all events
       ds = ROOT.uParticle( [k1,k2,pion] ) # create a candiate particle for reconstruction. using either positive or negative kaon
       # and a pion
 
-      bs_vtx = ROOT.uVertex([ds,pion])
-
-      bs = ROOT.uParticle( [ds,pion] )
-
       is_signal = is_from(k1, event, 431) and is_from(k2, event, 431) and is_from(pion, event,431)
       # particle id 431 is for the D+, checks if the positive kaon is from an event with a D+ present, checks if the negative 
       # kaon is from an event with a D+ present, checks if the pions are from events with D+ present. (See diagram)
@@ -128,56 +120,62 @@ for event in events: # loop through all events
       if k1.pt() + k2.pt() + pion.pt() < 1800 : continue # insufficient momentum to create a phi, discard
       if ds.mass < 1800 or ds.mass  > 2100 : continue # insufficient mass to create D particle, discard
 
-      pv  = ds.bpv_4d( event.Vertices ) # pv: primary vertex, finds best possible vertex for the considered
+      pv  = ds.bpv_4d( event.Vertices ) # pv: possible vertex, finds best possible vertex for the considered
       # particle (minimum Chi squared) 
 
-      b_pv = bs.bpv_4d(event.Vertices)
-
-
 #      vtx_chi2.Fill( ds_vtx.chi2_distance(pv), is_signal )
-      if ds_vtx.chi2_distance(pv) and bs_vtx.chi2_distance(b_pv) < 50 : continue # if the product of the Chi squareds of the particle and the vertex
+      if ds_vtx.chi2_distance(pv) < 50 : continue # if the product of the Chi squareds of the particle and the vertex
       # is greater than 50, discard
-      if dira_bpv(ds,event.Vertices,0.050) and dira_bpv(bs,event.Vertices,0.050) < 0.9 : continue # if the cos of the angle between momenta is less than 0.9 discard
-      
-      def vertex_distance(vertex1,vertex2):
-        d = lambda i: getattr(vertex1, i) - getattr(vertex2, i) # vertex1.i - vertex2.i
-        return( np.sqrt(d('x')**2 + d('y')**2 + d('z')**2))
-      
+      if dira_bpv(ds,event.Vertices,0.050)  < 0.9 : continue # if the cos of the angle between momenta is less than 0.9 discard
 
-      if vertex_distance(b_pv,pv) > 0.0 : continue
-      if bs_vtx.chi2 / bs_vtx.ndof > 5 : continue 
-      if bs.mass < 2300 or bs.mass  > 2700 : continue 
-
-      bs_chi2.Fill( bs_vtx.chi2 / bs_vtx.ndof, is_signal)
+      dp_candidate = ROOT.combine( phi, pion, doca_cut, 15, int(1))
       
-                  
+      # dm_candidate = ROOT.combine( ds, pi, doca_cut, 15, -1)
+      for pion2 in good_pions:
+        for p1, p2, d, d_vtx in dp_candidate:
+          bs_vtx = ROOT.uVertex( [p1, p2, pion2] )
+          bs = ROOT.uParticle( [p1,p2,pion2] )
+          is_b_signal = is_from(k1, event, 431) and is_from(k2, event, 431) and is_from(pion, event,431) and is_from(pion2, event,431)
+
+          b_vtx_chi2.Fill( bs_vtx.chi2 / bs_vtx.ndof, is_b_signal)
+          if bs_vtx.chi2 / bs_vtx.ndof > 5 : continue # if the chi2/ndf is not acceptable, disgard possible particle
+          if p1.pt() + p2.pt() + pion2.pt() < 1800 : continue # insufficient momentum to create a phi, discard
+          if bs.mass < 1800 or bs.mass  > 9000 : continue
+
+          b_pv  = bs.bpv_4d( event.Vertices )
+          if bs_vtx.chi2_distance(b_pv) < 50 : continue 
+          if dira_bpv(bs,event.Vertices,0.050)  < 0.9 : continue
+          print(bs.mass)
+          b_plot.Fill(bs.mass * 0.001)
+
+
+
+
+
+      print(ds.mass)
       # if is_signal : 
-      ds_plot.Fill( ds.mass * 0.001) # found the allowed D particle and adds to the mass plot (see equations)
-      bs_plot.Fill( bs.mass * 0.001)
-      
-            #found_signal |= is_signal 
-      #if not is_signal : # if not a signal its background, print this
-        #print( "Background")
-        #print_mc_particle( k1, event.MCParticles) 
-        #print_mc_particle( k2, event.MCParticles) 
-        #print_mc_particle( pion, event.MCParticles) 
+      plot.Fill(ds.mass * 0.001) # found the allowed D particle and adds to the mass plot (see equations)
+      found_signal |= is_signal 
+      # if not is_signal : # if not a signal its background, print this
+        # print( "Background")
+        # print_mc_particle( k1, event.MCParticles) 
+        # print_mc_particle( k2, event.MCParticles) 
+        # print_mc_particle( pion, event.MCParticles) 
 
   n_signal = n_signal + found_signal 
 
 
-
-
-# Plotting
+### Plotting 
 
 canvas = ROOT.TCanvas("canvas")
 canvas.cd()
-bs_plot.Draw()
-canvas.Print("m_bs50.pdf")
+plot.Draw()
+canvas.Print("outputs/m_ds50.pdf")
 
-
-canvas2 = ROOT.TCanvas("canvas")
-bs_chi2.Draw()
-canvas2.Print("b_vtx_chi2.pdf")
+chi2_plot = ROOT.TCanvas("canvas")
+chi2_plot.cd()
+vtx_chi2.Draw()
+chi2_plot.Print("outputs/chi2_plot.pdf")
 
 
 #print( n_signal ) 
