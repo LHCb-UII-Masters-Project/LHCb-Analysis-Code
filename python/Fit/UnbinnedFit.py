bdecaf896a14337525afc31197fbac747fa25154
Jack McQueen <jxm1366@student.bham.ac.uk>
2024-12-09 13:59:09 +0000

    Bold figures

--- python/Fit/UnbinnedFit.py
+++ python/Fit/UnbinnedFit.py
@@ -1,6 +1,6 @@
 # region IMPORTS
 import ROOT
-from Unfixedtwohundread import *
+from Variables50psPrelim import *
 from ROOT import TH1D, TH2D, TCanvas, TChain, TTree, TString, TFile,gInterpreter,gSystem,RooMinimizer
 from math import * 
 import sys
@@ -133,10 +133,10 @@ alphaR = ROOT.RooRealVar("alphaR", "cut off gauss right", variables['alphaR']['v
 nL = ROOT.RooRealVar("n1", "nleft of DCB", variables['nL']['value'], variables['nL']['min'], variables['nL']['max'])  # First power law exponent estimate
 nR = ROOT.RooRealVar("n2", "nright of DCB", variables['nR']['value'], variables['nR']['min'], variables['nR']['max'])  # Second power law exponent estimate
 
-alphaL.setConstant(True)
-alphaR.setConstant(True)
-nL.setConstant(True)
-nR.setConstant(True)
+#alphaL.setConstant(True)
+#alphaR.setConstant(True)
+#nL.setConstant(True)
+#nR.setConstant(True)
 
 sig = ROOT.RooCrystalBall("sig", "double crystal ball", x, mu, sigma, alphaL, nL, alphaR, nR)
 decay_constant = ROOT.RooRealVar("decay_constant", "decay_constant", variables['decay_constant']['value'], variables['decay_constant']['min'], variables['decay_constant']['max'])
@@ -169,7 +169,7 @@ frame1 = x.frame()
 frame1.SetTitle("")
 data.plotOn(frame1,ROOT.RooFit.Name("data"),ROOT.RooFit.Binning(number_of_bins))
 model.plotOn(frame1,ROOT.RooFit.Name("sig+bkg"), ROOT.RooFit.LineColor(ROOT.kBlue), ROOT.RooFit.LineStyle(ROOT.kSolid))
-model.plotOn(frame1, ROOT.RooFit.Components("bkg"),ROOT.RooFit.Name("bkg"), ROOT.RooFit.LineColor(ROOT.kCyan),ROOT.RooFit.LineStyle(ROOT.kDashed))
+model.plotOn(frame1, ROOT.RooFit.Components("bkg"),ROOT.RooFit.Name("bkg"), ROOT.RooFit.LineColor(ROOT.kMagenta),ROOT.RooFit.LineStyle(ROOT.kDashed))
 model.plotOn(frame1, ROOT.RooFit.Components("sig"),ROOT.RooFit.Name("sig"), ROOT.RooFit.LineColor(ROOT.kRed), ROOT.RooFit.LineStyle(ROOT.kDotted),ROOT.RooFit.LineStyle(ROOT.kDotted))  # Overall DCB
 
 chi2 = frame1.chiSquare("sig+bkg", "data",9)
@@ -195,28 +195,35 @@ with LHCbStyle() as lbs:
     c.cd(1)
     latex = ROOT.TLatex() 
     latex.SetNDC() 
-    latex.SetTextSize(0.050)     
+    latex.SetTextSize(0.050)  
+    latex.SetTextFont(62)
     ROOT.gPad.SetLeftMargin(0.15)
     ROOT.gPad.SetLogy() # Turn on logarithmic scale for Y-axis
     ROOT.gStyle.SetLineScalePS(1.2)
     frame1.GetYaxis().SetTitle("Entries/ (10 MeV/c^{2})")
     frame1.GetXaxis().SetTitle("m(B_{s}^{0}) [GeV/c^{2}]")
-    frame1.GetYaxis().SetTitleOffset(1)
+    frame1.GetYaxis().SetTitleOffset(0.9)
     frame1.GetXaxis().SetTitleOffset(1)
+    frame1.GetYaxis().SetTitleFont(62) 
+    frame1.GetXaxis().SetTitleFont(62)
+    frame1.GetYaxis().SetTitleSize(0.06) # Increase this value to make the font size larger
+    frame1.GetXaxis().SetTitleSize(0.06) # Increase this value to make the font size larger
+    frame1.GetXaxis().SetLabelSize(0.05)
+    frame1.GetYaxis().SetLabelSize(0.05)
+    frame1.GetXaxis().SetLabelFont(62)
+    frame1.GetYaxis().SetLabelFont(62)
 
-    frame1.GetYaxis().SetTitleSize(0.05) # Increase this value to make the font size larger
-    frame1.GetXaxis().SetTitleSize(0.05) # Increase this value to make the font size larger
     frame1.Draw()
 
 
     # Add the legend with LaTeX formatting, without a legend box, and matching LaTeX font
-    legend = ROOT.TLegend(0.70, 0.70, 0.975, 0.920) 
+    legend = ROOT.TLegend(0.67, 0.7, 0.97, 0.915) 
     legend.SetLineColor(0)  # Remove the legend border
     legend.SetLineStyle(0)  # Ensure no border line style
     legend.SetLineWidth(0)  # Set line width to 0
     legend.SetFillColor(0)  # Remove any fill color
     legend.SetFillStyle(0)  # Ensure no fill style
-    legend.SetTextFont(42)  # Helvetica, normal
+    legend.SetTextFont(62)  # Helvetica, normal
     legend.SetTextSize(0.045)  # Adjust text size as needed
 
 
@@ -241,10 +248,11 @@ with LHCbStyle() as lbs:
 
     latex2 = ROOT.TLatex() 
     latex2.SetNDC() 
-    latex2.SetTextSize(0.04)  
+    latex2.SetTextSize(0.045)  
+    latex2.SetTextFont(62)
     plot_time = time.strftime("%d %m %y", time.localtime())
 
-    latex2.DrawLatex(0.1, 0.09, f"E.Walsh ({plot_time})")
+    latex2.DrawLatex(0.1, 0.077, f"J.McQueen ({plot_time})")
 
 
 
@@ -255,10 +263,16 @@ with LHCbStyle() as lbs:
     ROOT.gPad.SetLeftMargin(0.15)
     frame2.GetYaxis().SetTitle("Pulls")
     frame2.GetXaxis().SetTitle("m(B_{s}^{0}) [GeV/c^{2}]")
-    frame2.GetYaxis().SetTitleOffset(1)
+    frame2.GetYaxis().SetTitleOffset(0.8)
     frame2.GetXaxis().SetTitleOffset(1)
-    frame2.GetYaxis().SetTitleSize(0.05) # Increase this value to make the font size larger
-    frame2.GetXaxis().SetTitleSize(0.05) # Increase this value to make the font size larger
+    frame2.GetYaxis().SetTitleSize(0.06) # Increase this value to make the font size larger
+    frame2.GetXaxis().SetTitleSize(0.06) # Increase this value to make the font size larger
+    frame2.GetYaxis().SetTitleFont(62) 
+    frame2.GetXaxis().SetTitleFont(62)
+    frame2.GetXaxis().SetLabelSize(0.05)
+    frame2.GetYaxis().SetLabelSize(0.05)
+    frame2.GetXaxis().SetLabelFont(62)
+    frame2.GetYaxis().SetLabelFont(62)
     frame2.Draw()
     line.Draw("same")
     
