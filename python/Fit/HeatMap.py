f0ffaf48f947373c724a8351a9219d51d207b718
Baggie270 <euan68@gmail.com>
2025-01-27 16:46:11 +0000

    Old heatmap (pre-meeting)

--- python/Fit/HeatMap.py
+++ python/Fit/HeatMap.py
@@ -6,6 +6,9 @@ import sys
 from os import path, listdir 
 from matplotlib.backends.backend_pdf import PdfPages
 
+# mode = "Efficacy"
+mode = "SNR"
+
 args = sys.argv
 file = args[1]
 
@@ -19,7 +22,10 @@ df = pd.read_csv(csv_file)
 # Extract relevant columns (adjust index for zero-based indexing)
 x = df.iloc[:, 1]  # Column 2 (index 1)
 y = df.iloc[:, 2]  # Column 3 (index 2)
-heat = df.iloc[:, 5]  # Column 6 (index 5)
+if mode == "SNR":
+    heat = (df.iloc[:, 5] * 504) / df.iloc[:, [9, 10, 11]].min(axis=1)
+elif mode == "Efficacy":
+    heat = (df.iloc[:,5])
 
 # Combine columns into a new DataFrame
 heatmap_data = pd.DataFrame({'x': x, 'y': y, 'heat': heat})
@@ -30,12 +36,11 @@ heatmap_pivot = heatmap_data.pivot_table(index='y', columns='x', values='heat', 
 # Plot the heatmap
 plt.figure(figsize=(10, 8))
 sns.heatmap(heatmap_pivot, cmap='viridis', annot=False, cbar=True)
-plt.title("IPChi2 0 Heat Map")
+plt.title(f"{file[-7:-4]}{mode} Heat Map")
 plt.xlabel("Transverse Momentum (MeV/c)")
 plt.ylabel("Total Momentum (MeV/c)")
 
-output_pdf = f"{file[:-18]}HeatMap{file[-7:-4]}.pdf"  # Replace with your desired output filename
+output_pdf = f"{file[:-18]}{mode}HeatMap{file[-7:-4]}.pdf"  # Replace with your desired output filename
 with PdfPages(output_pdf) as pdf:
     pdf.savefig()  # Save the current figure
     plt.close()  # Close the figure to avoid displaying it
-
