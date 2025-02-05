1399b995b738de3ef9eca0c6e22a0c45045c95f3
Baggie270 <euan68@gmail.com>
2025-02-05 11:42:25 +0000

    ClearOut

--- python/Outputs/BatchOutputs/Test_07-11-24_12:56:47/TestRun.py
+++
@@ -1,3 +0,0 @@
-print("hello")
-with open("t=300/test.txt","w") as f:
-    f.write("hello file")
--- python/Outputs/BatchOutputs/Test_07-11-24_13:02:39/TestRun.py
+++
@@ -1,3 +0,0 @@
-print("hello")
-with open("/home/user293/Documents/selections/python/t=300/test.txt","w") as f:
-    f.write("hello file")
