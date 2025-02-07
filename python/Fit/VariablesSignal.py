362924273f1eeecc8309f87c90a1ed6d1b8a0bd4
Jack McQueen <jxm1366@student.bham.ac.uk>
2025-02-07 15:31:40 +0000

    Signal fitting complete with new programmes for signal fitting, workspaces and root files for each fit of lamdac mass and xiccpp mass

---
+++ python/Fit/VariablesSignal.py
@@ -0,0 +1,90 @@
+import ROOT
+# Assuming variables.py contains the initial guesses, mins, and maxes
+variables = {
+    'mu': {'value': 2286.46, 
+           'min': 2270, 'max': 4000},
+    
+    'sigma': {'value': 2.476, 
+              'min': 1, 'max': 4},
+    
+    'alphaL': {'value': 1.6, 
+               'min': 1, 'max': 3},
+    
+    'alphaR': {'value': 1.4, 
+               'min': 1, 'max': 2},
+    
+    'nL': {'value': 6.3, 
+           'min': 6, 'max': 6.6},
+    
+    'nR': {'value': 5.7, 
+           'min': 5.3, 'max': 6.1}
+    
+}
+
+# Create the ROOT TTree
+fit_initial_guess_tree = ROOT.TTree("fit_initial_guess", "fit_initial_guess_tree")
+
+# Create the vectors for initial values, mins, and maxes
+mean_guess = ROOT.std.vector('float')()
+sigma_guess = ROOT.std.vector('float')()
+alphaL_guess = ROOT.std.vector('float')()
+alphaR_guess = ROOT.std.vector('float')()
+nL_guess = ROOT.std.vector('float')()
+nR_guess = ROOT.std.vector('float')()
+mean_min = ROOT.std.vector('float')()
+sigma_min = ROOT.std.vector('float')()
+alphaL_min = ROOT.std.vector('float')()
+alphaR_min = ROOT.std.vector('float')()
+nL_min = ROOT.std.vector('float')()
+nR_min = ROOT.std.vector('float')()
+mean_max = ROOT.std.vector('float')()
+sigma_max = ROOT.std.vector('float')()
+alphaL_max = ROOT.std.vector('float')()
+alphaR_max = ROOT.std.vector('float')()
+nL_max = ROOT.std.vector('float')()
+nR_max = ROOT.std.vector('float')()
+# Add the values to the vectors
+mean_guess.push_back(variables['mu']['value'])
+sigma_guess.push_back(variables['sigma']['value'])
+alphaL_guess.push_back(variables['alphaL']['value'])
+alphaR_guess.push_back(variables['alphaR']['value'])
+nL_guess.push_back(variables['nL']['value'])
+nR_guess.push_back(variables['nR']['value'])
+
+mean_min.push_back(variables['mu']['min'])
+sigma_min.push_back(variables['sigma']['min'])
+alphaL_min.push_back(variables['alphaL']['min'])
+alphaR_min.push_back(variables['alphaR']['min'])
+nL_min.push_back(variables['nL']['min'])
+nR_min.push_back(variables['nR']['min'])
+
+mean_max.push_back(variables['mu']['max'])
+sigma_max.push_back(variables['sigma']['max'])
+alphaL_max.push_back(variables['alphaL']['max'])
+alphaR_max.push_back(variables['alphaR']['max'])
+nL_max.push_back(variables['nL']['max'])
+nR_max.push_back(variables['nR']['max'])
+
+# Create branches in the TTree for guesses, mins, and maxes
+fit_initial_guess_tree.Branch("mean_guess", mean_guess)
+fit_initial_guess_tree.Branch("sigma_guess", sigma_guess)
+fit_initial_guess_tree.Branch("alphaL_guess", alphaL_guess)
+fit_initial_guess_tree.Branch("alphaR_guess", alphaR_guess)
+fit_initial_guess_tree.Branch("nL_guess", nL_guess)
+fit_initial_guess_tree.Branch("nR_guess", nR_guess)
+fit_initial_guess_tree.Branch("mean_min", mean_min)
+fit_initial_guess_tree.Branch("sigma_min", sigma_min)
+fit_initial_guess_tree.Branch("alphaL_min", alphaL_min)
+fit_initial_guess_tree.Branch("alphaR_min", alphaR_min)
+fit_initial_guess_tree.Branch("nL_min", nL_min)
+fit_initial_guess_tree.Branch("nR_min", nR_min)
+
+fit_initial_guess_tree.Branch("mean_max", mean_max)
+fit_initial_guess_tree.Branch("sigma_max", sigma_max)
+fit_initial_guess_tree.Branch("alphaL_max", alphaL_max)
+fit_initial_guess_tree.Branch("alphaR_max", alphaR_max)
+fit_initial_guess_tree.Branch("nL_max", nL_max)
+fit_initial_guess_tree.Branch("nR_max", nR_max)
+
+# Fill the tree (usually you would do this inside an event loop)
+fit_initial_guess_tree.Fill()
