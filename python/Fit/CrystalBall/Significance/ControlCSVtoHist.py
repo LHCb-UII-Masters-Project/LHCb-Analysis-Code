ba36230f39ff4a9423e587390a761d7be63940ae
Baggie270 <euan68@gmail.com>
2025-03-13 19:57:50 +0000

    Control mode done

--- python/Fit/CrystalBall/Significance/ControlCSVtoHist.py
+++ python/Fit/CrystalBall/Significance/ControlCSVtoHist.py
@@ -4,7 +4,7 @@ import numpy as np
 from ROOT import TCanvas, TH1D, TLine, TLegend
 
 # Read CSV
-csv_file = "/home/user293/Documents/selections/python/Outputs/ToyPlots/Models100Velo50.csv"
+csv_file = "/home/user293/Documents/selections/python/Outputs/ToyPlots/Models1000Velo50.csv"
 df = pd.read_csv(csv_file)
 
 # Extract values
@@ -17,7 +17,7 @@ percent_above_3 = np.mean(significances > 3) * 100
 percent_above_5 = np.mean(significances > 5) * 100
 
 # Define histogram
-num_bins = 30
+num_bins = 24
 hist = TH1D("Legend", "Control Mode Significance Distribution", num_bins,
             min(significances, default=0), max(significances, default=10))
 energy_range = (max(significances, default=10) - min(significances, default=0)) / num_bins
@@ -28,9 +28,25 @@ for significance in significances:
 
 # Draw histogram
 canvas = TCanvas("canvas", "Significance Histogram", 800, 600)
+hist.SetStats(False)
+
+# Make Hist Stylish
+hist.SetTitle("")
+hist.SetLineWidth(2)
 hist.GetXaxis().SetTitle("Significance")
 hist.GetYaxis().SetTitle(f"Entries / Bin (Bin Size = {energy_range:.2f})")
-hist.SetStats(False)
+# Apply the font and size to axes
+hist.GetYaxis().SetTitleOffset(0.82)
+hist.GetXaxis().SetTitleOffset(0.8)
+hist.GetYaxis().SetTitleFont(62)
+hist.GetXaxis().SetTitleFont(62)
+hist.GetYaxis().SetTitleSize(0.06)  # Increase title font size
+hist.GetXaxis().SetTitleSize(0.06)  # Increase title font size
+    # Axis label styling
+hist.GetXaxis().SetLabelSize(0.05)  # Label size
+hist.GetYaxis().SetLabelSize(0.05)  # Label size
+hist.GetXaxis().SetLabelFont(62)  # Label font
+hist.GetYaxis().SetLabelFont(62)  # Label font
 
 # Vertical lines
 line_secondpercentile = TLine(ninefive_percentile, 0, ninefive_percentile, hist.GetMaximum())
@@ -45,12 +61,12 @@ line_secondpercentile.SetLineStyle(2)
 line_signif3.SetLineStyle(2)
 line_signif5.SetLineStyle(2)
 
-line_secondpercentile.SetLineWidth(2)
-line_signif3.SetLineWidth(2)
-line_signif5.SetLineWidth(2)
+line_secondpercentile.SetLineWidth(5)
+line_signif3.SetLineWidth(5)
+line_signif5.SetLineWidth(5)
 
 # Legend
-legend = TLegend(0.66, 0.7, 0.97, 0.9)
+legend = TLegend(0.61, 0.7, 0.97, 0.9)
 legend.SetLineColor(0)
 legend.SetLineStyle(0)
 legend.SetLineWidth(0)
@@ -58,7 +74,7 @@ legend.SetFillColor(0)
 legend.SetFillStyle(0)
 legend.SetFillStyle(0)
 legend.SetTextFont(62)
-legend.SetTextSize(0.04)
+legend.SetTextSize(0.05)
 legend.AddEntry(hist, "Toy Models", "l")
 legend.AddEntry(line_secondpercentile, "95% CI", "l")
 if np.min(significances) < 5:
@@ -69,7 +85,9 @@ if np.min(significances) < 5:
 # Draw elements
 hist.Draw("HIST")
 line_secondpercentile.Draw()
-line_signif3.Draw()
-line_signif5.Draw()
+if np.min(significances) < 5:
+    line_signif5.Draw()
+    if np.min(significances) < 3:
+        line_signif3.Draw()
 legend.Draw()
-canvas.SaveAs("/home/user293/Documents/selections/python/Outputs/ToyPlots/Models100Velo50.pdf")
+canvas.SaveAs("/home/user293/Documents/selections/python/Outputs/ToyPlots/Models1000Velo50.pdf")
